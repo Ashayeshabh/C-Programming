36a22591140e9e954868d6ba5856d77e8ff10ce9
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-02-23 15:38:19 +0530

    Add files via upload

---
+++ WAP in C to calculate the addtion, subtraction, multiplication and division of two values 100 and 50.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b,c,d,e,f,g;
+   a=100;
+   b=50;
+   c=a+b;
+   d=a-b;
+   e=a*b;
+   f=a/b;
+   g=a%b;
+   printf("%d%d%d%d%d",c,d,e,f,g);
+   return 0;
+}
---
+++ WAP in C to calculate the area of square, rectangle, triangle and circle using certain required values.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   float as,s,ar,l,b,at,b1,h,ac,r;
+   s=10;
+   l=5;
+   b=3;
+   b1=15;
+   h=6;
+   r=7;
+   as=s*s;
+   ar=l*b;
+   at=0.5*b1*h;
+   ac=3.14*r*r;
+   printf("%f%f%f%f%f%f",s,l,b,b1,h,r);
+   return 0;
+}

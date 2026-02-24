1911af8d45c0682a766afa0a5e788b7c1f573342
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-02-24 17:34:27 +0530

    Add files via upload

---
+++ WAP in C display the output as Happy Holi in first line 2026 in second line nd IIBM in third line..c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Happy Holi\n");
+    printf("\t2026\n");
+    printf("\t\tIIBM");
+    return 0;
+}
---
+++ WAP in C to print a message Happy Holi in first line and 2026 in second line..c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Happy Holi \n 2026");
+    return 0;
+}

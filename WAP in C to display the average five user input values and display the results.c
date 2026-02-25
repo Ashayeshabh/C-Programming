ef40ba3a897f621941e0ac2e9aec2ecac7aee185
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-02-25 16:23:57 +0530

    Add files via upload

---
+++ WAP in C to display the average five user input values and display the results.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    f=(a+b+c+d+e)/5;
+    printf("%d",f);
+    return 0;
+}
+
---
+++ WAP in C to display the three time value of user input and display the result.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a,&b);
+    b=3*a;
+    printf("%d",b);
+    return 0;
+}
+

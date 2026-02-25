b9ba3e136f0db06fa7794d341542c1dc379b191c
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-02-25 16:15:17 +0530

    Add files via upload

---
+++ WAP in C to add and display two values 50 and 100 as a message The addition of two values 50 and 100 is = 150.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    a=50;
+    b=100;
+    c=a+b;
+    printf("The addition of two values %d and %d is = %d",a,b,c);
+    return 0;
+}
+

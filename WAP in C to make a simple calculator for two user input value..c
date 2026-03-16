c635e668c1eab6d7ce70182ada5ec9519b0c8d34
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-03-16 15:30:29 +0530

    Add files via upload

---
+++ WAP in C to make a simple calculator for two user input value..c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("%d%d%d%d",c,d,e,f);
+    return 0;
+}

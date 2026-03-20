bbf0ef9150e68cd672359ef9d795318f170e25b0
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-03-20 19:31:48 +0530

    Add files via upload

---
+++ WAP in C to display the total consumed electricity bill of a building in a particular month provided by the building owner by using Else-if Statement..c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x,y;
+    printf("Enter Consumed Unit");
+    scanf("%d",&x);
+    if (x>=1 && x<=100)
+    {
+        y=x*4.70;
+        printf("%d",y);
+    }
+    else if(x>=101 && x<=300)
+    {
+        y=x*5.20;
+        printf("%d",y);
+    }
+    else if(x>=301 && x<=500)
+    {
+        y=x*7.10;
+        printf("%d",y);
+    }
+    else if(x>=501 && x<=700)
+    {
+        y=x*9.20;
+        printf("%d",y);
+    }
+    else
+    {
+        y=x*10.40;
+        printf("%d",y);
+    }
+    return 0;
+}
